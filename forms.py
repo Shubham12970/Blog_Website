d2b1006663093f68520e40d04671a5cf4bfa68f2
Shubham <shubhamtur@outlook.com>
2024-09-05 16:32:26 +0530

    Initial Commit2

--- forms.py
+++ forms.py
@@ -13,7 +13,6 @@ class CreatePostForm(FlaskForm):
     submit = SubmitField("Submit Post")
 
 
-# TODO: Create a RegisterForm to register new users
 class RegisterForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
@@ -21,14 +20,12 @@ class RegisterForm(FlaskForm):
     submit = SubmitField("Submit Post")
 
 
-# TODO: Create a LoginForm to login existing users
 class LoginForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired(), Email()])
     password = PasswordField("Password", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
 
 
-# TODO: Create a CommentForm so users can leave comments below posts
 class CommentForm(FlaskForm):
     comment = CKEditorField("Comment", validators=[DataRequired()])
     submit = SubmitField("Submit Comment")
